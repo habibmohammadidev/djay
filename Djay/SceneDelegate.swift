cd874fe18b45138a639f0da486e1b33ea6fa84b9
HamedMohammadi <hamed.nova@gmail.com>
2025-11-27 12:54:49 +0100

    chore: Update app configuration
    - Update Info.plist with font configurations
    - Update LaunchScreen storyboard
    - Update SceneDelegate and ViewController

--- Djay/SceneDelegate.swift
+++ Djay/SceneDelegate.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
         self.window = window
+        
+        let coordinator = AppCoordinator(window: window)
+        appCoordinator = coordinator
+        coordinator.start()
     }
 }
--- Djay/ViewController.swift
+++ Djay/ViewController.swift
@@ -7,36 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupUI()
-    }
-    
-    private func setupUI() {
-        view.backgroundColor = .systemBackground
-        
-        let imageView = UIImageView(image: UIImage(systemName: "globe"))
-        imageView.preferredSymbolConfiguration = UIImage.SymbolConfiguration(scale: .large)
-        imageView.tintColor = .tintColor
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let label = UILabel()
-        label.text = "Hello, world!"
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        let stackView = UIStackView(arrangedSubviews: [imageView, label])
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        stackView.alignment = .center
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(stackView)
-        
-        NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-    }
-}
+//class ViewController: UIViewController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setupUI()
+//    }
+//    
+//    private func setupUI() {
+//        view.backgroundColor = .systemBackground
+//        
+//        let gradientView = GradientView()
+//        gradientView.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        view.addSubview(gradientView)
+//        
+//        NSLayoutConstraint.activate([
+//            gradientView.widthAnchor.constraint(equalTo: view.widthAnchor),
+//            gradientView.heightAnchor.constraint(equalTo: view.heightAnchor),
+//            gradientView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            gradientView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+//        ])
+//    }
+//}
